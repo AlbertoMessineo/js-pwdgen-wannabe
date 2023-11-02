ce87036b8ab406156b2e4085cf482c6e9b76bb2d
AlbertoMessineo <albevorox@gmail.com>
2023-11-02 16:59:42 +0100

    feat: prompts done

--- myscript.js
+++ myscript.js
@@ -1,4 +1,29 @@
 'use strict'
 
 
-const elementNome= document.getElementById('nome')
+const elementNome= document.getElementById('nome');
+const elementCognome= document.getElementById('cognome');
+const elementColorepreferito= document.getElementById('colorepreferito');
+let x = Math.random() * 100;
+const elementPassword= elementCognome + elementNome + elementColorepreferito + x;
+
+
+
+console.log(elementNome);
+console.log(elementCognome);
+console.log(elementColorepreferito);
+
+elementNome.innerHTML = " Il tuo nome: ";
+elementCognome.innerHTML = " Il tuo cognome: ";
+elementColorepreferito.innerHTML = " Il tuo colore preferito: ";
+
+
+
+elementNome.innerHTML += prompt('Inserisci il tuo nome');
+elementCognome.innerHTML += prompt('Inserisci il tuo cognome');
+elementColorepreferito.innerHTML += prompt('Inserisci il tuo colore preferito');
+
+
+
+
+
